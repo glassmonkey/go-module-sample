501cd3f3738bc9507ac9df3c0c0d1d7a62719a92
永野峻輔 <peace.infi0407@gmail.com>
2024-02-19 12:04:58 +0900

    fix

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 //go:generate go run golang.org/x/tools/cmd/stringer -type=Pill
-//go:generate go run go.uber.org/mock/mockgen -source=$GOFILE -package=mock_$GOPACKAGE -destination=mock/$GOPACKAGE/mock_$GOFILE
+//go:generate go run go.uber.org/mock/mockgen -source=$GOFILE -package=mock_$GOPACKAGE -destination=mock_$GOPACKAGE/mock_$GOFILE
 
 type Pill int
 
